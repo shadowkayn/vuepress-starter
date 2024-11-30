e4f38008cf85ee3f141548b7590bdf437cf4b2a0
shadowkayn <1669080561@qq.com>
2024-12-01 01:29:14 +0800

    fix

--- docs/.vuepress/.temp/pages/index.html.js
+++ docs/.vuepress/.temp/pages/index.html.js
@@ -1,16 +1,3 @@
 import comp from "E:/blogs/vuepress-starter/docs/.vuepress/.temp/pages/index.html.vue"
 const data = JSON.parse("{\"path\":\"/\",\"title\":\"KAYN Blogs\",\"lang\":\"en-US\",\"frontmatter\":{},\"headers\":[{\"level\":3,\"title\":\"网站使用vuePress搭建，具体可参考VuePress快速上手\",\"slug\":\"网站使用vuepress搭建-具体可参考vuepress快速上手\",\"link\":\"#网站使用vuepress搭建-具体可参考vuepress快速上手\",\"children\":[]}],\"git\":{\"updatedTime\":1732958783000,\"contributors\":[{\"name\":\"shadowkayn\",\"email\":\"1669080561@qq.com\",\"commits\":1}]},\"filePathRelative\":\"README.md\"}")
 export { comp, data }
-
-if (import.meta.webpackHot) {
-  import.meta.webpackHot.accept()
-  if (__VUE_HMR_RUNTIME__.updatePageData) {
-    __VUE_HMR_RUNTIME__.updatePageData(data)
-  }
-}
-
-if (import.meta.hot) {
-  import.meta.hot.accept(({ data }) => {
-    __VUE_HMR_RUNTIME__.updatePageData(data)
-  })
-}
