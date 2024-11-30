42ed3ad0972edbedaad17f695a04f8de7aaf1c09
shadowkayn <1669080561@qq.com>
2024-12-01 01:46:54 +0800

    fix

--- docs/.vuepress/.temp/pages/css/knowledge.html.js
+++ docs/.vuepress/.temp/pages/css/knowledge.html.js
@@ -1,16 +1,3 @@
 import comp from "E:/blogs/vuepress-starter/docs/.vuepress/.temp/pages/css/knowledge.html.vue"
 const data = JSON.parse("{\"path\":\"/css/knowledge.html\",\"title\":\"Knowledge\",\"lang\":\"en-US\",\"frontmatter\":{},\"headers\":[],\"git\":{\"updatedTime\":1732958783000,\"contributors\":[{\"name\":\"shadowkayn\",\"email\":\"1669080561@qq.com\",\"commits\":1}]},\"filePathRelative\":\"css/knowledge.md\"}")
 export { comp, data }
-
-if (import.meta.webpackHot) {
-  import.meta.webpackHot.accept()
-  if (__VUE_HMR_RUNTIME__.updatePageData) {
-    __VUE_HMR_RUNTIME__.updatePageData(data)
-  }
-}
-
-if (import.meta.hot) {
-  import.meta.hot.accept(({ data }) => {
-    __VUE_HMR_RUNTIME__.updatePageData(data)
-  })
-}
