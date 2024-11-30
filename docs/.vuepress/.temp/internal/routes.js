762e6a3485d53a31e9ae556e4c06b76eba2481a4
shadowkayn <1669080561@qq.com>
2024-12-01 01:09:34 +0800

    unknow

---
+++ docs/.vuepress/.temp/internal/routes.js
@@ -0,0 +1,29 @@
+export const redirects = JSON.parse("{}")
+
+export const routes = Object.fromEntries([
+  ["/css/knowledge.html", { loader: () => import(/* webpackChunkName: "css_knowledge.html" */"E:/blogs/vuepress-starter/docs/.vuepress/.temp/pages/css/knowledge.html.js"), meta: {"title":"Knowledge"} }],
+  ["/404.html", { loader: () => import(/* webpackChunkName: "404.html" */"E:/blogs/vuepress-starter/docs/.vuepress/.temp/pages/404.html.js"), meta: {"title":""} }],
+]);
+
+if (import.meta.webpackHot) {
+  import.meta.webpackHot.accept()
+  if (__VUE_HMR_RUNTIME__.updateRoutes) {
+    __VUE_HMR_RUNTIME__.updateRoutes(routes)
+  }
+  if (__VUE_HMR_RUNTIME__.updateRedirects) {
+    __VUE_HMR_RUNTIME__.updateRedirects(redirects)
+  }
+}
+
+if (import.meta.hot) {
+  import.meta.hot.accept(({ routes, redirects }) => {
+    __VUE_HMR_RUNTIME__.updateRoutes(routes)
+    __VUE_HMR_RUNTIME__.updateRedirects(redirects)
+  })
+}
+f (import.meta.hot) {
+  import.meta.hot.accept(({ routes, redirects }) => {
+    __VUE_HMR_RUNTIME__.updateRoutes(routes)
+    __VUE_HMR_RUNTIME__.updateRedirects(redirects)
+  })
+}
