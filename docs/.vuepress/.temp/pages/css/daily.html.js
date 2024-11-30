8c660b7933dcb78bee4be666e3fec927ab56a665
shadowkayn <1669080561@qq.com>
2024-11-30 18:13:18 +0800

    fix some config

--- docs/.vuepress/.temp/pages/css/daily.html.js
+++ docs/.vuepress/.temp/pages/css/daily.html.js
@@ -1,3 +1,3 @@
 import comp from "E:/blogs/vuepress-starter/docs/.vuepress/.temp/pages/css/daily.html.vue"
-const data = JSON.parse("{\"path\":\"/css/daily.html\",\"title\":\"Q&A\",\"lang\":\"en-US\",\"frontmatter\":{},\"headers\":[],\"git\":{\"updatedTime\":1731944050000,\"contributors\":[{\"name\":\"shadowkayn\",\"email\":\"1669080561@qq.com\",\"commits\":1}]},\"filePathRelative\":\"css/daily.md\"}")
+const data = JSON.parse("{\"path\":\"/css/daily.html\",\"title\":\"Q&A\",\"lang\":\"en-US\",\"frontmatter\":{},\"headers\":[],\"git\":{\"updatedTime\":1732958783000,\"contributors\":[{\"name\":\"shadowkayn\",\"email\":\"1669080561@qq.com\",\"commits\":1}]},\"filePathRelative\":\"css/daily.md\"}")
 export { comp, data }
--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -6,6 +6,7 @@ import { defineUserConfig } from 'vuepress'
 export default defineUserConfig({
     bundler: viteBundler(),
     theme: defaultTheme({
+            base:'kayn_blogs',
             lang:'zh-CN',
             title: 'KAYN Blogs',
             description: '个人博客-用来学习记录',
